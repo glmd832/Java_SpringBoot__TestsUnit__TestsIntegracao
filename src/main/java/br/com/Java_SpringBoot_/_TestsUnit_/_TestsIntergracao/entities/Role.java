e6bf9c75581620127efe43ca61730cd50793f489
Guilherme Almeida <glmd832@gmail.com>
2022-09-26 20:36:27 -0300

    entities

---
+++ src/main/java/br/com/Java_SpringBoot_/_TestsUnit_/_TestsIntergracao/entities/Role.java
@@ -0,0 +1,39 @@
+package br.com.Java_SpringBoot_._TestsUnit_._TestsIntergracao.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "table_role")
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Integer id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
